19aaeba12a0bec5e2e34753baaab392a6f2297b8
Sonnuy12 <kazanskij516@gmail.com>
2025-01-03 14:52:36 +0300

    Initial Commit

---
+++ DoctorsList/DoctorsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsListApp.swift
+//  DoctorsList
+//
+//  Created by сонный on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
